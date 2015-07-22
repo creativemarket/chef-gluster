cfd560d0f7df563a9ce45c02cd5c6e517dd07cf7
Jeremy Koerber <jeremy@creativemarket.com>
2015-07-22 15:05:33 -0700

    Gutted. New maintainer.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'gluster'
-maintainer       'Biola University'
-maintainer_email 'jared.king@biola.edu'
+maintainer       'Creative Market'
+maintainer_email 'jeremy@creativemarket.com'
 license          'Apache 2.0'
 description      'Installs and configures Gluster servers and clients'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.2.1'
+version          '4.0.0'
 depends          'apt', '>= 2.0'
 depends          'yum', '>= 3.0'
